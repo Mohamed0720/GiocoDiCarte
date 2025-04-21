7261dd38acf67e703b25209d80ef920d17b2f492
Mohamed <mohamedmouhassine7@gmail.com>
2025-04-21 22:29:07 +0200

    mostra carte al clic

--- GiocoDiCarte/GiocoDiCarte/Form1.cs
+++ GiocoDiCarte/GiocoDiCarte/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,23 +13,39 @@ namespace GiocoDiCarte
 {
     public partial class Form1 : Form
     {
-        //Variabili generali
+
+        //Classe Carte//-------------------------------------------------------------//
+        private class Carta
+        {
+            public Rectangle rect { get; set; }
+            public bool girato { get; set; }
+            public Bitmap colore { get; set; }
+
+            public Carta(Rectangle rect)
+            {
+                this.rect = rect;
+                this.girato = false;
+            }
+        }
+
+        //Variabili generali//-------------------------------------------------------//
         private bool inGame = false;
-        private List<Bitmap> carte = new List<Bitmap>();
         private Bitmap retroCarta = new Bitmap("../../Sprite/retroCarta.png");
-        List<Bitmap> carteGenerate = new List<Bitmap>();
-        private List<Rectangle> posSprite= new List<Rectangle>();
-        private bool paintedFirstTime = false;
+        private Bitmap cartaArancio = new Bitmap("../../Sprite/cartaArancio.png");
+        private Bitmap cartaRosso = new Bitmap("../../Sprite/cartaRosso.png");
+        private Bitmap cartaBlu = new Bitmap("../../Sprite/cartaBlu.png");
+        private Bitmap cartaVerde = new Bitmap("../../Sprite/cartaVerde.png");
+        private List<Carta> carte = new List<Carta>();
 
 
         public Form1()                                                              
         {                                                                           
             InitializeComponent();                                                  
             menuPanel.BringToFront();                                               
-        }                                                                           
+        }
 
 
-        //tasto Gioca
+        //tasto Gioca//--------------------------------------------------------------//
         private void tastoGioca_Click(object sender, EventArgs e)
         {
             menuPanel.Hide();
@@ -36,15 +53,22 @@ namespace GiocoDiCarte
             inGame = true;
 
 
-            //Creazione delle sprite delle carte
-            for(int i = 1; i < 5; i++)
+            //Creazione delle sprite delle carte//-----------------------------------//
+            float width = panelGame.Width / 2;
+            float height = panelGame.Height / 2;
+            int padding = 20;
+
+            for (int i = 0; i < 2; i++)
             {
-                carte.Add(new Bitmap($"../../Sprite/carta{i}.png"));
-                carte.Add(new Bitmap($"../../Sprite/carta{i}.png"));
+                for(int j = 0; j < 4; j++)
+                {
+                    Rectangle r = new Rectangle(Convert.ToInt32((width - (retroCarta.Width + padding*3/2)) + (retroCarta.Width/2*j + padding*j/2)), Convert.ToInt32((height - (retroCarta.Height/2 + padding/2)) + (retroCarta.Height/2*i + padding*i)), retroCarta.Width/2, retroCarta.Height/2);
+                    carte.Add(new Carta(r));
+                }
             }
 
-            //----------------------------------------------------------
-            //Generazione casuale dell'ordine delle carte
+
+            //Generazione casuale dell'ordine delle carte//--------------------------//
             List<int> esclusi = new List<int>();
             Random oggettoRand = new Random();
             int cartaCasuale;
@@ -54,73 +78,82 @@ namespace GiocoDiCarte
                 do
                 {
                     cartaCasuale = oggettoRand.Next(0, 9);
-                    carteGenerate.Add(carte[oggettoRand.Next(carte.Count)]);
 
                     if (esclusi.Contains(cartaCasuale))
                     {
-                        carteGenerate.RemoveAt(i);
+                        Console.WriteLine("C'è già");
+                    } else
+                    {
+                        if (cartaCasuale == 1 || cartaCasuale == 2)
+                        {
+                            carte[i].colore = cartaArancio;
+                        }
+                        else if (cartaCasuale == 3 || cartaCasuale == 4)
+                        {
+                            carte[i].colore = cartaRosso;
+                        }
+                        else if (cartaCasuale == 5 || cartaCasuale == 6)
+                        {
+                            carte[i].colore = cartaBlu;
+                        }
+                        else if (cartaCasuale == 7 || cartaCasuale == 8)
+                        {
+                            carte[i].colore = cartaVerde;
+                        }
                     }
                 } while (esclusi.Contains(cartaCasuale));
                  
                 esclusi.Add(cartaCasuale);
             }
+
+            Invalidate();
         }
 
-        //Tasto Esci
+        //Tasto Esci//---------------------------------------------------------------//
         private void tastoEsci_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        //Tasto Torna al Menu Principale
+        //Tasto Torna al Menu Principale//-------------------------------------------//
         private void Indietro_Click(object sender, EventArgs e)
         {
             panelGame.Hide();
             menuPanel.Show();
             inGame = false;
 
-            for (int i = 0; i < 8; i++)
-            {
-                carte[i].Dispose();
-            }
         }
-        
-        //Funzione chiamata ad ogni ridisegno del pannello di gioco
+
+        //Funzione chiamata ad ogni ridisegno del pannello di gioco//----------------//
         private void panelGame_Paint(object sender, PaintEventArgs e)
         {
-            float width = panelGame.Width / 2;
-            float height = panelGame.Height / 2;
-            int padding = 20;
 
             if (inGame)
             {
-                for(int i = 0; i < 2; i++)
+                for(int i = 0; i < 8; i++)
                 {
-                    for(int j = 0; j < 4; j++)
+                    if (carte[i].girato)
                     {
-                        if (paintedFirstTime)
-                        {
-                            Rectangle rect = new Rectangle(Convert.ToInt32(width - (carte[j].Width + padding * 2)) + (j * carte[j].Width / 2 + (j * padding)), Convert.ToInt32((height - (carte[i].Height / 2 + padding)) + (i * carte[i].Height / 2 + (i * padding / 2))), carte[j].Width / 2, carte[i].Height / 2);
-                            posSprite.Add(rect);
-                        }
-                        
-                        e.Graphics.DrawImage(retroCarta, posSprite[j+(5*i)]);
+                        e.Graphics.DrawImage(carte[i].colore, carte[i].rect);
+                    }
+                    else
+                    {
+                        e.Graphics.DrawImage(retroCarta, carte[i].rect);
                     }
                 }
                 
             }
-
-            paintedFirstTime = true;
+            
         }
 
-        //Quando il mouse sta su una carta
+        //Quando il mouse sta su una carta//-----------------------------------------//
         private void panelGame_MouseMove(object sender, MouseEventArgs e)
         {
             if (inGame)
             {
                 for (int i = 0; i < 8; i++)
                 {
-                    if (posSprite[i].Contains(e.Location))
+                    if (carte[i].rect.Contains(e.Location))
                     {
 
                     }
@@ -128,16 +161,17 @@ namespace GiocoDiCarte
             }
         }
 
-        //Quando si preme col mouse su una carta
+        //Quando si preme col mouse su una carta//-----------------------------------//
         private void panelGame_MouseClick(object sender, MouseEventArgs e)
         {
             if (inGame)
             {
                 for (int i = 0; i < 8; i++)
                 {
-                    if (posSprite[i].Contains(e.Location))
+                    if (carte[i].rect.Contains(e.Location))
                     {
-                        e.Graphics.DrawImage(carteGenerate[i], posSprite[i].X, posSprite[i].Y, posSprite[i].Width, posSprite[i].Height);
+                        carte[i].girato = true;
+                        Invalidate();
                     }
                 }
             }
